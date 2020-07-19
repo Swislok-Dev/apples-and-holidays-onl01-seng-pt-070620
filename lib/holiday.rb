333aa6a1fee48cc9108837685ee0448f9c01cc74
Cory Marker <cmarker4@gmail.com>
2020-07-19 05:45:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,9 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, day|
     day.map do |day, supply|
-      if supply.include?("BBQ")
-        day
-      end
+      day if supply.include?("BBQ")
     end
   end
 end
