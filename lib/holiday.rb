957886de59e63cb079f53b226602b5cdfd3be107
Cory Marker <cmarker4@gmail.com>
2020-07-19 05:35:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,9 @@ def all_supplies_in_holidays(holiday_hash)
       #{supply.join(", ")}"
     end
   end
-all_supplies_in_holidays(holiday_hash)
 end
+all_supplies_in_holidays(holiday_hash)
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
