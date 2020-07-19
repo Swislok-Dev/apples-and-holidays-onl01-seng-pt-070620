bf8b3a551b171724822aee3ef01b049802283e7a
Cory Marker <cmarker4@gmail.com>
2020-07-19 05:42:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,5 +76,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.map do |season, day|
+    season[day].map do |day, supply|
+      day if day[supply].include?("BBQ")
+    end
+  end
 end
